8248575400ca6163216a124158f4a832d99fff2b
Nazri Abdullah <nazri.abdullah@me.com>
2023-08-27 18:34:08 +0200

    updated production app url

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,10 +1,8 @@
-const BASE_URL = 'http://155.4.109.218';
-const WEB_PORT = 3000;
-const API_PORT = 7777;
-export const WEB_URL = `${BASE_URL}:${WEB_PORT}`;
-export const WEB_TEXT_DECRYPTION_URL = `${BASE_URL}:${WEB_PORT}/t/d/`;
-export const WEB_FILE_DECRYPTION_URL = `${BASE_URL}:${WEB_PORT}/f/d/`;
-export const WEB_TEXT_DECRYPTION_URL_WITH_PIN = `${BASE_URL}:${WEB_PORT}/t/d/p/`;
+const BASE_URL = 'http://129.151.202.16';
+const API_PORT = '/api';
+export const WEB_TEXT_DECRYPTION_URL = `${BASE_URL}/t/d/`;
+export const WEB_FILE_DECRYPTION_URL = `${BASE_URL}/f/d/`;
+export const WEB_TEXT_DECRYPTION_URL_WITH_PIN = `${BASE_URL}/t/d/p/`;
 export const TEXT_API_URL = `${BASE_URL}:${API_PORT}/secret`;
 export const FILE_API_URL = `${BASE_URL}:${API_PORT}/file`;
 export const EMAIL_API_URL = `${BASE_URL}:${API_PORT}/email`;
