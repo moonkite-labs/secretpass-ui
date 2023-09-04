7b18498101360f523e465fd91861a298a8d9253d
Nazri Abdullah <nazri.abdullah@me.com>
2023-09-04 21:23:26 +0200

    updated api address

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://129.151.202.16';
+const BASE_URL = 'http://155.4.109.218';
 const API_PORT = '/api';
 export const WEB_TEXT_DECRYPTION_URL = `${BASE_URL}/t/d/`;
 export const WEB_FILE_DECRYPTION_URL = `${BASE_URL}/f/d/`;
 export const WEB_TEXT_DECRYPTION_URL_WITH_PIN = `${BASE_URL}/t/d/p/`;
-export const TEXT_API_URL = `${BASE_URL}:${API_PORT}/secret`;
-export const FILE_API_URL = `${BASE_URL}:${API_PORT}/file`;
-export const EMAIL_API_URL = `${BASE_URL}:${API_PORT}/email`;
+export const TEXT_API_URL = `${BASE_URL}${API_PORT}/secret`;
+export const FILE_API_URL = `${BASE_URL}${API_PORT}/file`;
+export const EMAIL_API_URL = `${BASE_URL}${API_PORT}/email`;
